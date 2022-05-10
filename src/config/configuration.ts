d6d89984f93849c203aa7967d8215aec832a43ed
Ruslan Lukmanov <rlukmanov@saminfotech.com>
2022-05-10 14:41:58 +0400

    CONFIG

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,11 +6,13 @@ const YAML_CONFIG_FILENAME = 'config.yml';
 const YAML_CONFIG_OVERWRITE_FILENAME = 'config.overwrite.yml';
 
 export default () => {
-  const filename = existsSync(join(__dirname, YAML_CONFIG_OVERWRITE_FILENAME))
+  const filename = existsSync(
+    join(__dirname, '..', '..', YAML_CONFIG_OVERWRITE_FILENAME),
+  )
     ? YAML_CONFIG_OVERWRITE_FILENAME
     : YAML_CONFIG_FILENAME;
-  return yaml.load(readFileSync(join(__dirname, filename), 'utf8')) as Record<
-    string,
-    any
-  >;
+
+  return yaml.load(
+    readFileSync(join(__dirname, '..', '..', filename), 'utf8'),
+  ) as Record<string, any>;
 };
